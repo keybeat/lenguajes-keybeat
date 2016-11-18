1f01bbf8244ddea4c2b780e7874c37dd3b22015a
T-107 <T-107@PC259>
2016-11-17 19:40:18 -0600

    Mi primer pushero

--- src/main/java/concurrencia/Principal.java
+++ src/main/java/concurrencia/Principal.java
@@ -5,6 +5,9 @@
  */
 package concurrencia;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author T-107
@@ -29,8 +32,16 @@ public class Principal implements Runnable {
     Thread t3= new Thread(new Runnable(){
         @Override
         public void run() {
-           
-        }
+            while(true){
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+            }
+         
+            System.out.println("Soy un background Thread..");
+            }
+            }
         
     });
     t3.start();
