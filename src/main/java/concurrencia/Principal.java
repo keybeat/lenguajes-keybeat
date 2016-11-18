d0c9aafd7258e22e0ff31bc947ed583808e6978b
T-107 <T-107@PC259>
2016-11-17 19:11:05 -0600

    Mi primer pushero

---
+++ src/main/java/concurrencia/Principal.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package concurrencia;
+
+/**
+ *
+ * @author T-107
+ */
+public class Principal implements Runnable {
+    //SEGUNDA FORMA DE CREAR THREADS IMPLEMENTANTO LA INTERFACE 
+    //RUNNABLE USANDO POLIMORFISMO
+    public static void main(String[] args) {
+    //Runnable --> referencia de la clase
+    Runnable r=new Principal();
+    Thread t1 = new Thread(r);
+    t1.setName("Juan");
+    Thread t2 = new Thread(r);
+    t2.setName("Ana");
+    //t1.start();
+    //t2.start();
+    
+    
+    //TERCERA FORMA QUE VIENE EN EL EXAMEN
+    //USANDO CLASES INTERNAS ANONIMAS
+    
+    Thread t3= new Thread(new Runnable(){
+        @Override
+        public void run() {
+           
+        }
+        
+    });
+    t3.start();
+    
+    
+    
+    
+    }
+    
+
+    
+
+    //@Override;
+    public void run() {
+        /*
+        System.out.println("Soy un Thread y me llamo "+ Thread.currentThread().getName());   
+        if(Thread.currentThread().getName().equals("Ana")){
+            System.out.println("Soy mujer");
+        }else{
+                    System.out.println("Soy Hombre");
+        }
+        
+        }
+        */
+    }
+    
+}
